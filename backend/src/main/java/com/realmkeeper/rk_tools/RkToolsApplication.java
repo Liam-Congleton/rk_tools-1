4949db5eca3179a258ecf236e53f0b2bb673ca73
Liam Congleton <greenonions117@gmail.com>
2023-10-27 13:45:48 -0400

    initial

---
+++ backend/src/main/java/com/realmkeeper/rk_tools/RkToolsApplication.java
@@ -0,0 +1,13 @@
+package com.realmkeeper.rk_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkToolsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/realmkeeper/rk_tools/RkToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realmkeeper.rk_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
