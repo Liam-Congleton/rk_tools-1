814ae25b68d4d7815123300dd7c980d4df380516
Liam Congleton <greenonions117@gmail.com>
2023-11-01 13:29:22 -0400

    maven configuration

--- backend/src/main/com/realmkeeper/rk_tools/RkToolsApplication.java
+++ backend/src/main/com/realmkeeper/rk_tools/RkToolsApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RkToolsApplication {
+public class RkToolsApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(RkToolsApplication.class, args);
+		
 	}
 
 }
